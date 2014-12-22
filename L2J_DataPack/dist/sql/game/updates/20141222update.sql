d02a76d0a919fcdc687062e1b112b0f48a070cd7
Salmelu <salmelu@gmail.com>
2014-12-22 20:23:06 +0100

    TI htmls fixed, removed duplicate spawn

--- L2J_DataPack/dist/sql/game/updates/20141222update.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO spawnlist VALUES
-("16_24", 1, 33246, -114769, 259561, -1192, 0, 0, 42862, 60, 0, 0, 0), -- Eleve
-("16_24", 1, 33241, -114821, 258990, -1192, 0, 0, 42862, 60, 0, 0, 0); -- Devno
-
-DELETE FROM spawnlist WHERE locy = 255680; -- Delete double LeBont
